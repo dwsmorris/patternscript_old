1c8e0908895943fa0b38fc2b13c39d68fc41c58c
Darryl Morris <dwsmorris@users.noreply.github.com>
2017-03-01 23:01:10 +0000

    compiles basic test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var _ = require("lodash/fp");
+import _ from "lodash/fp";
 
 var script2lines = script => script.split("\n");
 var trim = line => line.trim();
@@ -23,6 +23,16 @@ var tokens2ast = tokens => {
 
 	return ast;
 };
+var set = function(name, operator, expression) {
+	this[name] = evaluate(expression);
+} 
+var rootEnvironment = () => ({
+	console: console,
+	set: set,
+	".": function(object, property) {
+		return this[object][property];
+	}
+});
 
 var script = 
 `log <= (. console log)
@@ -30,5 +40,12 @@ var script =
 `
 
 var output = script2lines(script).map(_.flow(trim, line2sexp, sexp2spaced, spaced2tokens, _.map(evaluateToken), tokens2ast));
+var environment = rootEnvironment();
+var evaluate = function(sexp) {
+	if (!sexp.length) return;
+
+	return environment[sexp[0]].apply(environment, sexp.slice(1));
+};
+_.each(evaluate, output);
 
 console.log(JSON.stringify(output));
